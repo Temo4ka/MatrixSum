39aa05fe71f9117b88e88e8bf93f10ebe4c43276
Temo4ka <artemy.bukin@yandex.ru>
2024-02-27 10:31:13 +0300

    print works

--- headers/matrix.h
+++ headers/matrix.h
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <assert.h>
+
+//-------------------------PRINT-------------------------------------
+template<typename... T>
+void Print() {};
+
+template<typename T0, typename... T1>
+void Print(const T0& Head, const T1&... Tail) {
+    std::cout << Head << ' ';
+    Print(Tail...);
+}
+//-------------------------------------------------------------------
 
 template<int... line> struct matrix_line {
     const size_t length = sizeof...(line);
+
+    void print() {
+        Print(line...);
+    }
 };
 
 using ZERO = matrix_line<>;
@@ -23,18 +39,47 @@ struct SUM<matrix_line<line1...>, matrix_line<line2...>> {
 };
 //-------------------------------------------------------------------
 
-//-------------------------PRINT-------------------------------------
 
-template<int Head, int... Tail>
-void print() {
-    std::cout << Head << ' ';
 
-    if (sizeof...(Tail)) print(Tail);
-}
+// template<typename... T>
+// void print(const matrix_line<>&) {}
+
+// template<int Head, int... Tail>
+// void print(const matrix_line<Head, Tail...> line) {
+//     std::cout << Head << ' ';
+
+//     matrix_line<Tail...> tail;
+//     print<Tail...>(tail);
+// }
+
+// template<typename... T> void print() { assert("cringanull!\n"); }
+
+// template<typename... T> void print<ZERO>() {}
+
+// template<int Head, int... Tail>
+// void print<matrix_line<Head, Tail...>>() {
+//     std::cout << Head << '\n';
+
+//     matrix_line<Tail...> tail;
+//     print<Tail...>();
+// }
+
+// template <typename... T> struct PRINT;
+
+// template <int Head, int... Tail>
+// struct PRINT<matrix_line<Head, Tail...>> {
+//     void print() {
+//         std::cout << Head << ' ';
+
+//         if (sizeof...(Tail) == 0) return;
+//         PRINT<matrix_line<Tail...>>::print();
+//     }
+// };
+
 //-------------------------------------------------------------------
 
 
-// template<int... line>
-// template<matrix_line<line>... lines> struct matrix {
-
-// };
+template<int size>
+struct Matrix {
+    
+};
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 
 int main() {
 
-    using a = matrix_line<1, 2, 3>;
-    using b = matrix_line<1, 1, 1>;
-    using c = SUM<a, b>::RESULT;
+    using A = matrix_line<1, 2, 3>;
+    using B = matrix_line<1, 1, 1>;
+    using Sum = SUM<A, B>::RESULT;
 
-    
-    // print(std::cout, c);
+    Sum sum;
+    sum.print();
+
+    // SUM<matrix_line<1, 2, 3>, matrix_line<1, 1, 1>>::RESULT sum;
 
     return 0;
 }
